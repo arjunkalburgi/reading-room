33dcd015f392bf69e9ae0448e3a896633771beea
Arjun <askalburgi@gmail.com>
2025-08-12 17:33:21 -0400

    proj init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Arjun's Reading Room
+
+My goal in 2025 is to write more and after a few months of writing in Notion, I want a better space for irl friends and internet friends to interact with my writing. 
+
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<style src="$lib/styles/global.css" global></style>
+</svelte:head>
+
+{@render children?.()}
